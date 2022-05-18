2be57af2c8269127ff099817e57115454ffcfceb
ValeryAuchynnikau <VAuchynnikau@gomel.iba.by>
2022-05-18 17:40:30 +0300

    Add project files.

---
+++ Task 1/Mark.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Mark
+    {
+        public string SubjectName { get; set; }
+        public int Value { get; set; }
+
+        public Mark(string subjectName, int value)
+        {
+            this.SubjectName = subjectName;
+            this.Value = value;
+        }
+    }
+}
---
+++ Task 1/Student.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Student
+    {
+        Mark[] marks;
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public Mark[] Marks
+        {
+            get { return marks; }
+            set { marks = value; }
+
+        }
+
+
+        public Student(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+        }
+
+        public Student(string firstName, string lastName, Mark[] marks)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Marks = marks;
+        }
+
+        public double GetAvgMark()
+        {
+            return this.Marks.Average(m => m.Value);
+        }
+
+        public void ResetAllMarks()
+        {
+            foreach (Mark mark in Marks)
+            {
+                mark.Value = 0;
+            }
+        }
+    }
+}
