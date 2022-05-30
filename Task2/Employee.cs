0667d4cf807233e28d81ef02829c8b71b4afd6d9
ValeryAuchynnikau <VAuchynnikau@gomel.iba.by>
2022-05-30 14:05:55 +0300

    Implement sorting

---
+++ Task2/Employee.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Employee : IComparable<Employee>
+    {
+        public string PersonalInfo { get; set; }
+        public double Salary { get; set; }
+
+        public static List<Employee> SortBySalary(List<Employee> employees)
+        {
+            return employees.OrderBy(p => p.Salary).ToList();
+        }
+       
+        public int CompareTo(Employee employee)
+        {
+            if (employee == null)
+                return 1;
+
+            else
+                return this.PersonalInfo.CompareTo(employee.PersonalInfo);
+        }
+
+        public override string ToString()
+        {
+            return "Personal information" + PersonalInfo + " Salary - " + Salary;
+        }
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Employee> list = new List<Employee>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Employee employee = new Employee();
+                employee.PersonalInfo = "information" + random.Next(1, 100);
+                employee.Salary = random.Next(500, 1100);
+                list.Add(employee);
+            }
+
+            Console.WriteLine("Original");
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+
+            Console.WriteLine("Sort by salary");
+            list = Employee.SortBySalary(list);
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+
+            Console.WriteLine("Sort by information");
+            list.Sort();
+            foreach (Employee item in list)
+                Console.WriteLine(item.ToString());
+        }
+    }
+}
